6973ab5cc949f3485f675d119d2c4cd9b1682fed
jatinunreal007 <jatin.unreal07@gmail.com>
2026-03-17 10:24:58 +0000

    deleted:    projectLyla/Resources/build.py 	new file:   projectLyla/Resources/build_cpp.py 	new file:   projectLyla/Resources/setup_cpp.bat

---
+++ projectLyla/Resources/build_cpp.py
@@ -0,0 +1,32 @@
+import os  
+
+file_path = "/root/essentials/setup_cpp.bat"
+with open(file_path,'r') as file:
+	data = file.read()
+print("Hello sir ! ")
+scode_name = input("\nEnter The Name of Source Code file (Without extension): ")
+hfile_name = input("\nEnter The Name of Header file (Without extension): ")
+
+replacements = {
+	"scode_name": scode_name,
+	"hfile_name": hfile_name
+}
+
+for old, new in replacements.items():
+	data = data.replace(old, new)
+	
+with open(file_path,'w') as new_file:
+	new_file.write(data)
+	
+print("Execution Sucessfull !\n ")
+
+os.system("./essentials/setup_cpp.bat")   
+
+
+for old,new in replacements.items():
+	data = data.replace(new, old)
+	
+with open(file_path,'w') as new_file:
+	new_file.write(data)
+	
+print("\nshellScript ran Sucessfully ")
---
+++ projectLyla/Resources/setup_cpp.bat
@@ -0,0 +1,16 @@
+cp /root/essentials/setup_cpp.bat /root/essentials/logs
+
+cp /storage/emulated/0/Download/California/projectLyla/Source_Code/scode_name.cpp  /root/projectLyla/Source_Code
+
+cp /storage/emulated/0/Download/California/projectLyla/Source_Code/scode_name.cpp  /root/projectLyla/Linkers
+
+cp /storage/emulated/0/Download/California/projectLyla/Header_files/hfile_name.h  /root/projectLyla/Header_files
+
+cp /storage/emulated/0/Download/California/projectLyla/Header_files/hfile_name.h /root/projectLyla/Linkers
+
+g++ /root/projectLyla/Linkers/scode_name.cpp -o /root/essentials/exes/scode_name
+
+cd essentials
+cd exes
+
+./scode_name
