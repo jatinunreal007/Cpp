5e8b70e1bd1e4c27ed628ba983b80be486353cbe
jatinunreal007 <jatin.unreal07@gmail.com>
2026-03-19 15:28:22 +0000

    new file:   projectLyla/SourceCode/pL26.cpp 	new file:   projectLyla/essentials/exes/pL26 	new file:   projectLyla/essentials/linker/pL26.cpp

---
+++ projectLyla/SourceCode/pL26.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "h_01.h"
+
+//Macros in C++
+
+#define print(x) std::cout<< x <<std::endl; //----> This is macro
+
+//! but we should not use macros in this way(I will have a real world example in OpenGl code)
+int main()
+{
+	print(69); //---> It will just paste the code here 
+    std::cin.get();
+}
